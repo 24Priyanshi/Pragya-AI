5d717625b8d8b2186f5f66cba519debe6d4d8d84
24Priyanshi <24priyanshimishra@gmail.com>
2026-08-31 14:57:40 +0530

    kalam

--- src/app/(site)/kalamprotocol/page.tsx
+++ src/app/(site)/kalamprotocol/page.tsx
@@ -17,6 +17,13 @@ export default function KalamProtocolPage() {
   return (
     <PageShell>
       <PageHero hero={hero} />
+
+      {/* Placeholder overview poster under the hero, pending a proper redesign (added 2026-08-31) */}
+      <section className="mb-32">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img alt="Kalam Protocol overview poster" className="w-full h-auto" src="/kalamprotocol_poster.png" />
+      </section>
+
       <StatStrip stats={stats} />
 
       <section className="space-y-32">
